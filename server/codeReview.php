bf20fabb8ac57266e70555e1e511a877079ac88c
zhaojianhui <zhaojianhui129@163.com>
2017-02-13 17:44:08 +0800

    更新代码

---
+++ server/codeReview.php
@@ -0,0 +1,23 @@
+<?php
+//代码审计方法
+$currentFile = 'testClass.php';
+//$currentFile = '../apps/controllers/Home.php';
+require_once $currentFile;
+$currentFileContent = trim(file_get_contents($currentFile));
+
+preg_match_all('~^\s*(?:abstract\s+|final\s+)?(?:class|interface)\s+(\w+)(\s+(extends|implements)\s+(\w+))?\s*{~mi', $currentFileContent, $classes);
+foreach ($classes[1] as $key => $class) {
+    echo "====================class========================\r\n";
+    $rc = new ReflectionClass($class);
+    $methods = $rc->getMethods();
+    foreach ($methods as $method){
+        echo "======================method=========================\r\n";
+        $rm = new ReflectionMethod($method->class, $method->name);
+        $doc = $rm->getDocComment();
+        $matchDoc = [];
+        preg_match_all("#\\r\\n#", $doc, $matchDoc);
+        var_dump($rm->getDocComment(), count($matchDoc[0]) + 1);
+    }
+    var_dump($rc->getMethods());
+}
+var_dump($classes[1]);
---
+++ server/testClass.php
@@ -0,0 +1,27 @@
+<?php
+class test_a{
+    /**
+     * test function a
+     * @date 2017
+     */
+    public function func_a(){
+        $a = 123;
+    }
+
+    /**
+     * hilo
+     */
+    public function func_c(){
+        $b = 798;
+    }
+}
+
+class test_b{
+    /**
+     * test function b
+     * @date 2017
+     */
+    public function func_b(){
+        $a = 456;
+    }
+}
